e182e48544caeb154a12b20c80805fd0256e2c61
MareFalconinae <130992047+MareFalconinae@users.noreply.github.com>
2023-06-13 22:55:31 +0300

    Update TruckTransportation.java

--- Project2_3/src/main/java/com/mycompany/project2_3/TruckTransportation.java
+++ Project2_3/src/main/java/com/mycompany/project2_3/TruckTransportation.java
@@ -3,10 +3,13 @@ package com.mycompany.project2_3;
 
 public class TruckTransportation extends Truck{
     private boolean isAbroad;
+
 public TruckTransportation(int id,int plateNumber,int numberOfTires,double dailyFee,double loadingCapacity,boolean isAbroad){
     super(id,plateNumber,numberOfTires,dailyFee,loadingCapacity);    
     this.isAbroad=isAbroad;
-}    
+}
+public TruckTransportation(){
+}
 
     public boolean isIsAbroad() {
         return isAbroad;
