90937c191528adc4e706972b020974daa95ceeb3
MareFalconinae <130992047+MareFalconinae@users.noreply.github.com>
2023-06-13 19:44:28 +0300

    Update TestVHS.java

--- Project2_3/src/main/java/com/mycompany/project2_3/TestVHS.java
+++ Project2_3/src/main/java/com/mycompany/project2_3/TestVHS.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.project2_3;
-
-/**
- *
- * @author Deniz
- */
+import java.util.Scanner;
 public class TestVHS {
     
+    public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    System.out.println("What is your role?");
+    System.out.println("1/Admin");
+    System.out.println("2/Customer");
+    int c=scn.nextInt();
+    switch(c){
+        case 1:
+            admin a=new admin();
+            a.AdminPanel();
+        case 2:    
+            customer s=new customer();
+            s.CustomerPanel();
+        default:            
+    }    
+}
 }
