f5200f7de7480ec71f698c7dab9de28a9be7df39
MareFalconinae <130992047+MareFalconinae@users.noreply.github.com>
2023-06-13 22:55:18 +0300

    Update TruckSmall.java

--- Project2_3/src/main/java/com/mycompany/project2_3/TruckSmall.java
+++ Project2_3/src/main/java/com/mycompany/project2_3/TruckSmall.java
@@ -1,9 +1,13 @@
 
+
 package com.mycompany.project2_3;
 
 public class TruckSmall extends Truck{
 
 public TruckSmall(int id,int plateNumber,int numberOfTires,double dailyFee,double loadingCapacity){
     super(id,plateNumber,numberOfTires,dailyFee,loadingCapacity);
-}    
 }
+public TruckSmall(){
+}
+}
+
