eb0490c9d0f9d399c1f6e117a2e644adb7a2636e
MareFalconinae <130992047+MareFalconinae@users.noreply.github.com>
2023-06-13 22:57:02 +0300

    Update customer.java

--- Project2_3/src/main/java/com/mycompany/project2_3/customer.java
+++ Project2_3/src/main/java/com/mycompany/project2_3/customer.java
@@ -1,7 +1,7 @@
-+package com.mycompany.project2_3;
+package com.mycompany.project2_3;
 import java.util.Scanner;
+import java.util.Date;
 public class customer {
-    
     VehiclePark vp=new VehiclePark();
     Scanner scn=new Scanner(System.in);
 public void CustomerPanel(){
@@ -19,25 +19,76 @@ public void CustomerPanel(){
         System.out.println("7-Drop a vehicle");
         System.out.println("8-Quit");    
     int c=scn.nextInt();
+    int id;
+    Date s;
+    Date f;
     switch(c){
         case 1:
             vp.displayAllVehicles();
         case 2:
             vp.displayAvailableVehicles();
         case 3:         
-        
-        case 4:
-            vp.bookVehicle(sc,s,f);
-            vp.bookVehicle(t,s,f);
+            
+        case 4:  
+            System.out.println("enter id:");
+            id=scn.nextInt();
+            System.out.println("enter beginning date:");
+            s=new Date();
+            System.out.println("enter final date");
+            f=new Date();
+            for(Vehicle v : vp.AllVehiclelist){
+            if(v.getId()==id){
+            if(v.getClass()==CarSport.class){
+            vp.bookVehicle((CarSport)v,s,f);
+            }
+            else if(v.getClass()==Truck.class){
+            vp.bookVehicle((Truck)v,s,f);    
+            }
+            else{
+            System.out.println("Not bookable");    
+            }    
+            }
+            }
         case 5:
-            vp.cancelVehicle(sc,s,c);
-            vp.cancelVehicle(t,s,c);
+            System.out.println("enter id:");
+            id=scn.nextInt();
+            System.out.println("enter beginning date:");
+            s=new Date();
+            System.out.println("enter canceling date:");
+            f=new Date();
+            for(Vehicle v : vp.AllVehiclelist){
+            if(v.getId()==id){
+            if(v.getClass()==Truck.class){
+            vp.cancelVehicle((Truck)v,s,f);
+            }
+            else{
+            vp.cancelVehicle((CarSport)v,s,f);
+            }
+            }
+            }
         case 6:
+            System.out.println("enter id:");
+            id=scn.nextInt();
+            System.out.println("enter beginning date:");
+            s=new Date();
+            System.out.println("enter final date:");
+            f=new Date();
+            for(Vehicle v : vp.AllVehiclelist){
+            if(v.getId()==id){ 
             vp.rentVehicle(v,s,f);
+            }
+            }
         case 7:
+            System.out.println("enter id:");
+            id=scn.nextInt();
+            for(Vehicle v : vp.AllVehiclelist){
+            if(v.getId()==id){ 
             vp.dropVehicle(v);
+            }
+            }
         case 8:
             System.exit(0);
     }   
     }    
 }        
+}
